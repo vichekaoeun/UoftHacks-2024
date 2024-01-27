aba455aa55625d58c208e7a5569b1a4ab9608eea
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-27 04:46:32 -0500

    boilerplate setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container-main'>
+      <div id='bar'>
+        <div className='bar-fill'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='content'>
+        <div className='content-item'>
+          <div id='item1'><h1>Hello World</h1></div>
+          <div id='item2'><h1>Hello World</h1></div>
+        </div>
+        <div className='content-item'>
+          <div id='item1'><h1>Hello World</h1></div>
+          <div id='item2'><h1>Hello World</h1></div>
+        </div>
+        <div className='content-item'>
+          <div id='item1'><h1>Hello World</h1></div>
+          <div id='item2'><h1>Hello World</h1></div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
