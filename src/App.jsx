9c4a08ebc0b73f8903b61cc049ffa7fe3988e627
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-27 11:59:07 -0500

    home page boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,36 @@ function App() {
   return (
     <div className='container-main'>
       <div id='bar'>
+        <div className='bar-text'>
+          <h1 id='home'>Home</h1>
+          <p>Welcome Back ____</p>
+        </div>
         <div className='bar-fill'></div>
       </div>
       <div id='content'>
         <div className='content-item'>
-          <div id='item1'><h1>Hello World</h1></div>
-          <div id='item2'><h1>Hello World</h1></div>
-        </div>
-        <div className='content-item'>
-          <div id='item1'><h1>Hello World</h1></div>
-          <div id='item2'><h1>Hello World</h1></div>
-        </div>
-        <div className='content-item'>
-          <div id='item1'><h1>Hello World</h1></div>
-          <div id='item2'><h1>Hello World</h1></div>
+          <div id='calendar-header'>
+            <h1>February</h1>
+            <div id='calendar-button'>
+              <button>
+                left
+              </button>
+              <button>right</button>
+            </div>
+          </div>
+          <div id='calendar'>
+          </div>
         </div>
       </div>
-    </div>
+      <div id='sidebar'>
+        <div className='content-item'>
+          <h1>Hello World</h1>
+        </div>
+      </div>
+      <div id='button-container'>
+        <div className="button_plus"></div>
+      </div>
+    </div >
   )
 }
 
