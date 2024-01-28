d8f65b07b55a860c2c29fc69120925f53a415518
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-01-27 21:16:55 -0500

    add submit and file upload

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Post.css'
 
 const Post = () => {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [caption, setCaption] = useState('');
+
+    // Function to handle file selection
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleCaptionChange = (event) => {
+        setCaption(event.target.value);
+    };
+
+    // Function to handle form submission
+    const handleSubmit = () => {
+        // Handle the selected file here
+        console.log('Selected file:', selectedFile);
+        console.log('Caption:', caption);
+        // You can upload the file to the server or perform other actions here
+    };
+
     return (
         <div className='post-container'>
             <div id='post-content'>
                 <div id='post-items'>
                     <div className='up-post'>
-                        <h1>Image</h1>
+                        <div>
+                            <h1>Upload a Photo</h1>
+                        </div>
+                        <div>
+                            <input
+                                className='upload-button'
+                                type="file"
+                                accept="image/*"
+                                onChange={handleFileSelect}
+                            />
+                        </div>
                         <div id='caption'>
                             <p>Caption</p>
-                            <input></input>
+                            <input className='caption-input'
+                                type="text"
+                                value={caption}
+                                onChange={handleCaptionChange}></input>
                         </div>
                     </div>
                 </div>
@@ -21,7 +56,7 @@ const Post = () => {
                 <h1>Create a Post</h1>
             </div>
             <div className='submit-button'>
-                <button>Submit</button>
+                <button onClick={handleSubmit}>Submit</button>
             </div>
         </div>
     )
